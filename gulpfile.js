8d7defc2bc15ff04fc44d00492306d00e6c77342
Neil Sarkar <neil.r.sarkar@gmail.com>
2014-11-08 11:26:05 -0800

    Fuck sourcemaps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
     sass = require('gulp-sass');
 
 var paths = {
@@ -18,18 +17,14 @@ var paths = {
 
 gulp.task('compile_scripts', function() {
   gulp.src(paths.js).
-    pipe(sourcemaps.init()).
     pipe(concat('application.js')).
-    pipe(sourcemaps.write()).
     pipe(gulp.dest('app/dist/'));
 });
 
 gulp.task('compile_stylesheets', function() {
   gulp.src(paths.css).
     pipe(sass()).
-    pipe(sourcemaps.init()).
     pipe(concat('application.css')).
-    pipe(sourcemaps.write()).
     pipe(gulp.dest('app/dist/'));
 });
 
