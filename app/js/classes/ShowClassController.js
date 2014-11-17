9c5c15f5b336f3be7afc8755ecd3ca7e7689c3a7
Neil Sarkar <neil.r.sarkar@gmail.com>
2014-11-16 22:07:40 -0800

    Add results preview to the picks page

--- app/js/classes/ShowClassController.js
+++ app/js/classes/ShowClassController.js
@@ -2,6 +2,7 @@ angular.module('classes').controller('ShowClassController', [
   '$scope', '$routeParams', 'classService', '$location', 'Choice',
   function($scope, $routeParams, classService, $location, Choice) {
     $scope.$root.bodyClass = 'classes-show'
+    $scope.participants = []
     $scope.class = {
       name: $routeParams.className
     }
@@ -25,6 +26,17 @@ angular.module('classes').controller('ShowClassController', [
       }
     }, function no() {
       window.alert("Nope.")
+    })
+
+    Choice.forClass($routeParams.className).$watch(function(event) {
+      if( event.event == 'child_added' ) {
+        $scope.participants.push(classService.slugToName(event.key))
+        $scope.abstainers = $scope.class.members.filter(function(member) {
+          return $scope.participants.indexOf(member.slug) < 0;
+        })
+      } else {
+        console.debug(event)
+      }
     })
 
     // TODO: clear choices before setting them here
